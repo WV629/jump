d5cc4849ff4ee2b5891857c83f24dd22eeb860a7
nickwu <2762882807@qq.com>
2023-07-07 14:50:02 +0800

    feat:增加访问限制

--- start.py
+++ start.py
@@ -1,22 +1,29 @@
-# -*- coding:utf-8 -*-
-
 from send import slenium_chrome
-# 2. 生成APP
 from flask import request, Flask
 from selenium_out_url import chrome_driver
+from threading import Lock
 
 app = Flask(__name__)
-browser = chrome_driver().selenium_out_url()
+lock = Lock()
+concurrent_requests = 0
 
-
-
-@app.route('/', methods = ["POST"])
+@app.route('/', methods=["POST"])
 def hello():
+    global concurrent_requests
+
+    with lock:
+        if concurrent_requests >= 5:
+            return "wait"
+        concurrent_requests += 1
+
+    browser = chrome_driver().selenium_out_url()
     url = request.form.get("url")
     out_utl = slenium_chrome(url, browser)
-    return out_utl
 
+    with lock:
+        concurrent_requests -= 1
+
+    return out_utl
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=35000, debug=False)
-
